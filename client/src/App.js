4cb7c865c8eba0bc226cb2cca92792de96addea6
shrivatsatrivedi <shrivatsatrivedi689@gmail.com>
2024-11-06 01:26:06 +0530

    homepage build
    initial structure and cc along with slight react router dom on link and register

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import Post from "./Post";
+import Header from "./Header";
+import { Route,Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <Routes>
+      <Route index element ={
+         <main>
+        
+         <Header />
+          <Post />
+          <Post />
+          <Post />
+      
+       </main>
+      } />
+      <Route path ={'/login'} element ={
+        <div>login</div>
+      } />
+      <Route path ={'/register'} element ={
+        <div>register</div>
+      } />
+    </Routes>
+     
+  );
+}
+export default App;
