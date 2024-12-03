3dc7bc888dee87da41c5be1f3b9f8cd4c787fb25
shrivatsatrivedi <shrivatsatrivedi689@gmail.com>
2024-12-04 01:32:21 +0530

    changed backend url

--- api/index.js
+++ api/index.js
@@ -331,4 +331,8 @@ app.get('/post/:id/image', async (req, res) => {
     }
 });
 
-console.log(`Server is running on blog-app-silk-gamma.vercel.app`);
+// Start the Server
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Server is running on blog-app-silk-gamma.vercel.app`);
+});
